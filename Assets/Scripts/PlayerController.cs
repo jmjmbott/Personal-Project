2401f9001fc03f1b64c9e05e8e821f335b78da74
Jordan <hunkyjerky@msn.com>
2021-07-15 21:08:50 -0600

    Player movement script
    Adding

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,52 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    private float speed = 10.0f;
+    private float topBound = 11;
+    private float bottomBound = -2;
+
+    private Rigidbody playerRb;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerRb = GetComponent<Rigidbody>();
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        MovePlayer();
+        ConstrainPlayerPosition();
+             
+    }  
+
+    //Moves player with arrow keys
+    void MovePlayer()
+    {
+        float horizontalInput = Input.GetAxis("Horizontal");
+        float verticalInput = Input.GetAxis("Vertical");
+
+        playerRb.AddForce(Vector3.forward * speed * verticalInput);
+        playerRb.AddForce(Vector3.right * speed * horizontalInput);
+
+    }
+
+    // Boundaries for restricted movement
+    void ConstrainPlayerPosition()
+    {
+        if (transform.position.z > topBound)
+        {
+            transform.position = new Vector3(transform.position.x, transform.position.y, topBound);
+        }
+        if (transform.position.z < bottomBound)
+        {
+            transform.position = new Vector3(transform.position.x, transform.position.y, bottomBound);
+        }
+    }
+}
